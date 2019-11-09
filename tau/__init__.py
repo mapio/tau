121e2cb6dac34ac0a5203eae519c3c4239baba9b
Massimo Santini <massimo.santini@gmail.com>
2019-11-09 19:27:42 +0100

    Improving the UI

--- tau/__init__.py
+++ tau/__init__.py
@@ -1,7 +1,10 @@
+from csv import reader
 from pathlib import Path
+from smtplib import SMTPException
 
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 from flask import Flask, flash, request, redirect, render_template, send_from_directory, url_for
+from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__, instance_relative_config = True)
@@ -11,16 +14,44 @@ app.config.from_mapping(
 )
 app.config.from_pyfile('config.py', silent = True)
 
+mail = Mail(app)
+
 USTS = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+UID2MAIL = dict(reader(
+    (Path(app.instance_path) / 'uid2email.tsv').open('r'), delimiter = '\t')
+)
 
 Path(app.instance_path).mkdir(exist_ok = True)
 
+@app.route('/uid2mail')
+def uid2mail():
+    return {'uid2email': UID2MAIL}
+
 @app.route("/", methods=['GET', 'POST'])
-def home():
+def index():
+    status = None
+    email = None
     if request.method == 'GET':
-        return render_template('reqtoken.html')
+        status = 'GET'
     else:
-        return USTS.dumps(request.form['uid'])
+        if not 'uid' in request.form:
+            status = 'MISSING_UID'
+        else:
+            uid = request.form['uid']
+            try:
+                email = UID2MAIL[uid]
+            except KeyError:
+                status = 'UNREGISTERED_UID'
+            else:
+                token = USTS.dumps(uid)
+                try:
+                    msg = Message('Your token is: {}'.format(token), recipients = ['massimno.santini@gmail.com'])
+                    #mail.send(msg)
+                    status = 'OK'
+                except SMTPException:
+                    status = 'SEND_ERROR'
+    return render_template('reqtoken.html', status = status, email = email)
+
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
@@ -30,23 +61,23 @@ def allowed_file(filename):
 
 @app.route('/upload/', methods=['GET'])
 @app.route('/upload/<string:token>', methods=['GET', 'POST'])
-def upload_file(token = None):
+def upload(token = None):
+    status = None
+    email = None
 
     if request.method == 'GET':
-        error = None
-        uid = None
         if not token:
-            error = 'MISSING'
+            status = 'MISSING_TOKEN'
         else:
             try:
                 uid = USTS.loads(token, max_age = 24 * 60 * 60 * 5)
             except SignatureExpired:
-                error = 'EXPIRED'
+                status = 'EXPIRED_TOKEN'
             except BadSignature:
-                error = 'INVALID'
-        return render_template('uploads.html', uid = uid, error = error)
+                status = 'INVALID_TOKEN'
 
-    elif request.method == 'POST':
+    else:
+
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -61,4 +92,7 @@ def upload_file(token = None):
             filename = secure_filename(file.filename)
             file.save(str(Path(app.config['UPLOAD_FOLDER']) / filename))
             return 'ok'
+
+    return render_template('uploads.html', email = email, status = status)
+
 
